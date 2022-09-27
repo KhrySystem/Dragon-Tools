0fc032f1a8c13de7457c2fe4f476c034f95e37a7
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-09-27 07:24:41 -0400

    Add CMake Macros & first test.

--- tests/openWindow.cpp
+++ tests/openWindow.cpp
@@ -5,7 +5,7 @@ int main(void)
 	if(dgInit() != DG_TRUE) {
 		return false;
 	}
-	dgWindow window = dgCreateWindow(1080, 960, "openWindow.c");
+	dgWindow window = dgCreateWindow(1080, 960, "openWindow.cpp");
 	return 0;
 	dgCloseWindow(window);
 }
