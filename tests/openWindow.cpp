2084b0d2b6c7a3bf71451429596283a4bb170a5a
Comp01-sudo <70709374+Comp01-sudo@users.noreply.github.com>
2022-10-05 09:38:16 -0400

    Add first test

--- tests/openWindow.cpp
+++ tests/openWindow.cpp
@@ -1,11 +1,40 @@
-#include <dragon/dragon.h>
+#include <dragon/dragon.hpp>
+#include <iostream>
+
+struct App {
+	App() {
+		dgInit("openWindow.cpp");
+
+		DgWindowCreateParams params{};
+		params.width = 800;
+		params.height = 600;
+		if(!dgCreateNewWindow(params)) {
+			throw std::exception("Window creation failed");
+		}
+	}
+
+	void run() {
+		while(!dgShouldWindowClose(0)) {
+			dgUpdate();
+		}
+		dgCloseWindow(0);
+	}
+};
 
 int main(void)
 {
-	if(dgInit() != DG_TRUE) {
-		return false;
+	App app;
+	try {
+		app = App();
+		app.run();
+	} catch(DgBaseException_T &e) {
+		if(const char** exc_info=boost::get_error_info<DgExceptionInfo>(e)) {
+			std::cerr << "Error Found: " << *exc_info << std::endl;
+		}
+	} catch(std::exception &e) {
+		std::cerr << "Non-Dragon Error found: " << e.what() << std::endl;
 	}
-	dgWindow window = dgCreateWindow(1080, 960, "openWindow.cpp");
-	return 0;
-	dgCloseWindow(window);
+
+	dgTerminate();
+	return 0x0000;
 }
