9757498fdc4818165285c79eef07c7e1548c63e3
Khrysys <admin@khrysystem.dev>
2022-10-21 15:08:04 -0400

    Add windows build scripts

---
+++ build/windows_msvc_2022.ps1
@@ -0,0 +1,13 @@
+# Assumes Boost and Vulkan are already installed and up-to-date on the system.
+
+git submodule update --init --recursive
+cmake . -Bbin -G"Visual Studio 16 2022"                     `
+    --no-warn-unused-cli                                    `
+    -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE               `
+    -DCMAKE_BUILD_TYPE=$args[0]                             `
+    -DDRAGON_INSTALL_DIR:String=$args[1]                    `
+    -DBOOSTROOT:Path=$args[2]                               `
+    -DVulkan_INCLUDE_DIR:Path=${VULKAN_SDK}/include         `
+    -DVulkan_LIBRARY:Path=${VULKAN_SDK}/bin/vulkan-1.lib    
+cmake --build $workspace --config $args[0]
+ctest -C $args[0]
